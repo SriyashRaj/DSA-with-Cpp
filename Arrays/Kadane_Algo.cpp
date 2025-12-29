5a26c5a34ffff93a1a684dc10af8eb273778ad7c
Sriyash Raj <sriyashraj001@gmail.com>
2025-12-29 11:54:39 +0530

    Implemented Kadane's Algorithm

--- Arrays/Kadane_Algo.cpp
+++ Arrays/Kadane_Algo.cpp
@@ -1,1 +1,30 @@
 
+/*
+Problem: Maximum Subarray Sum (Kadane's Algorithm)
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int currSum = arr[0];
+    int maxSum = arr[0];
+
+    for (int i = 1; i < n; i++) {
+        currSum = max(arr[i], currSum + arr[i]);
+        maxSum = max(maxSum, currSum);
+    }
+
+    cout << maxSum;
+    return 0;
+}
+
